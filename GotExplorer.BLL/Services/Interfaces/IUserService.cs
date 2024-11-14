22d3c8e17b2dfcef0ced3bddca84126fdee0f209
Andrii <andriy07da@gmail.com>
2024-11-14 11:33:03 +0200

    Added register and login endpoints

---
+++ GotExplorer.BLL/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using GotExplorer.BLL.DTOs;
+
+namespace GotExplorer.BLL.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<string> Register(RegisterDTO registerDTO);
+        Task<string> Login(LoginDTO loginDTO);
+    }
+}
