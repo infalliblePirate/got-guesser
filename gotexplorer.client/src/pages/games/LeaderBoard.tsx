4e01022afae3ca63999abff94d0e8f43a7039f17
Ulia <ulianaescha05@gmail.com>
2025-01-14 17:50:56 +0200

    StartGame page and LeaderBoard

--- gotexplorer.client/src/pages/games/LeaderBoard.tsx
+++ gotexplorer.client/src/pages/games/LeaderBoard.tsx
@@ -1,4 +1,39 @@
-const LeaderBoard = () => {
-    return (<h2>Leader Board</h2>);
+import './GameLevelPage.scss';
+
+interface Player {
+  rank: number;
+  name: string;
+  score: number;
 }
-export default LeaderBoard;
+
+interface LeaderboardProps {
+  players: Player[];
+}
+
+const Leaderboard: React.FC<LeaderboardProps> = ({ players }) => {
+  return (
+    <div className="leaderboard">
+      <h2 className="leaderboard__title">TOP PLAYERS</h2>
+      <table className="leaderboard__table">
+        <thead>
+          <tr>
+            <th className="leaderboard__header">RANK</th>
+            <th className="leaderboard__header">NAME</th>
+            <th className="leaderboard__header">SCORE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player) => (
+            <tr key={player.rank} className="leaderboard__row">
+              <td className="leaderboard__cell">{player.rank}</td>
+              <td className="leaderboard__cell">{player.name}</td>
+              <td className="leaderboard__cell">{player.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Leaderboard;
--- gotexplorer.client/src/pages/games/StartGamePage.tsx
+++ gotexplorer.client/src/pages/games/StartGamePage.tsx
@@ -1,9 +1,21 @@
-import LeaderBoard from "./LeaderBoard";
+import Footer from "../additional_components/Footer";
+import Navigation from "../additional_components/Navigation";
+import './StartGamePage.scss';
+import { useNavigate } from "react-router-dom";
 
 const StartGamePage = () => {
-    return (<>
-        <h1> Start Game Page</h1>
-        <LeaderBoard />
-    </>);
+
+    const navigate = useNavigate();
+    const handleGameClick = () => navigate("/lvl/game");
+    return (<div className="start-game-page">
+             <Navigation />
+             <div className="main-content">
+                 <div className="text">LET'S EXPLORE THE WORLD</div>
+                 <button className="button-start" onClick={handleGameClick}>START NOW</button>
+             </div>
+
+             <Footer />
+         </div>);
 }
+
 export default StartGamePage;
